d56070a6b434b36c07ff1891c964f33e6ea6a2e9
Ganesh <mganesh.951@gmail.com>
2017-09-12 17:37:31 +0530

    Quiz app completed

--- QuiziOS/ViewController.swift
+++ QuiziOS/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var questionText : UILabel!
+    @IBOutlet var answerText : UILabel!
+    
+    let questions : [String] = [
+        "What is 7+7 ?",
+        "What is the capital of vermont?",
+        "What is cognac made from?"
+    ]
+    
+    let answers : [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    var currentQuestionIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionText.text = questions [currentQuestionIndex]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func showNextQuestion(_ sender : UIButton) {
+        currentQuestionIndex+=1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions [currentQuestionIndex]
+        questionText.text = question
+        answerText.text = "???"
+    }
+    
+    @IBAction func showAnswer (_ sender : UIButton) {
+        let answer: String = answers [currentQuestionIndex]
+        answerText.text = answer
+    }
 }
 
